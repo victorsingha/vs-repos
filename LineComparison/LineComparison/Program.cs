1477d0f64f1845a276b1ff90c1007e86346d7366
victor-singha <vicunite2@gmail.com>
2021-04-20 17:59:17 +0530

    line comparison code

--- LineComparison/LineComparison/Program.cs
+++ LineComparison/LineComparison/Program.cs
@@ -8,8 +8,8 @@ namespace LineComparison
         {
             double x1=2 , x2=4, x3=3, x4=6, y1=6, y2=8, y3=2, y4=5;
 
-            double lenghtLine1 = Math.Sqrt(Math.Pow((x2-x1),2)+ Math.Pow((y2 - y1), 2));
-            double lenghtLine2 = Math.Sqrt(Math.Pow((x4 - x3), 2) + Math.Pow((y4 - y3), 2));
+            double lenghtLine1 = Math.Sqrt(Math.Pow((x2-x1),2)+ Math.Pow((y2-y1), 2));
+            double lenghtLine2 = Math.Sqrt(Math.Pow((x4-x3), 2) + Math.Pow((y4-y3), 2));
             
             int res1 = lenghtLine1.CompareTo(lenghtLine2);       
             if (res1 < 0) Console.WriteLine("line1 < line2");
